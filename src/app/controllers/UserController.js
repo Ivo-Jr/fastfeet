d99f6dc4c36dfc6c93d18288560101f494f9b391
Ivo-Jr <maciel.jr95@gmail.com>
2021-02-09 17:43:04 -0300

    email end passord changeable

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,17 +2,23 @@ import User from '../models/User';
 
 class UserController {
     async update(request, response) {
-        const { oldPassword } = request.body;
+        const { email, oldPassword } = request.body;
 
         const user = await User.findByPk(request.userId);
 
-        if (oldPassword && !(await user.checkPassword(oldPassword))) {
-            return response
-                .status(401)
-                .json({ error: 'Password does not match' });
+        if (email && email !== user.email) {
+            const userExists = await User.findOne({ where: { email } })
+
+            if (userExists) {
+                return response.status(400).json({ error: 'User already exists.' });
+            }
         }
 
-        const { id, name, email } = await user.update(request.body);
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return response.status(401).json({ error: 'Password does not match' });
+        }
+
+        const { id, name } = await user.update(request.body);
 
         return response.json({
             id,
